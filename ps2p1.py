efd5482d51dcad2d03e418859c4b7d66b326641a
Frank Alvino <71982017+frankalvino@users.noreply.github.com>
2022-09-26 18:54:49 -0500

    Add files via upload

---
+++ ps2p1.py
@@ -0,0 +1,9 @@
+#this is a comment
+qty = float(input("Please enter quantity ordered"))
+ppu = float(input("Please enter price per unit"))
+
+#process phase
+extprice = qty*ppu
+
+#output phase
+print("The extended price is $", extprice)
---
+++ ps2p2.py
@@ -0,0 +1,8 @@
+lname = input("Enter your last name")
+hours = float(input("Enter hours worked"))
+rate = float(input("Enter rate of pay"))
+
+gp = hours * rate
+
+#print(lname)
+print(lname, " Your gross pay is $", gp)
